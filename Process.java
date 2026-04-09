a9a4f09b762bc78bcf619f94fed0910e8f0c64cf
abdelrhmanSherpiny <abdelrahmanelshirpeny@gmail.com>
2026-04-09 10:15:03 +0200

    scheduler update

--- Process.java
+++ Process.java
@@ -8,13 +8,8 @@ public class Process {
 
     public int remainingTime;
     public int completionTime;
-    public int turnaroundTime;
-    public int waitingTime;
-
-    public String color;
 
     public Process(int burstTime, int priority, Integer arrivalTime) {
-
 
         Random rand = new Random();
         this.arrivalTime = (arrivalTime == null) ? rand.nextInt(11) : arrivalTime;
@@ -24,16 +19,20 @@ public class Process {
 
         this.remainingTime = burstTime;
         this.completionTime = 0;
-        this.turnaroundTime = 0;
-        this.waitingTime = 0;
 
-        // Generate random color for Gantt Chart
-        this.color = String.format("#%06x", rand.nextInt(0xFFFFFF + 1));
+    }
+
+    public void update_remaining(int currentTime) {
+        remainingTime--;
+        if (remainingTime == 0) {
+            completionTime = currentTime;
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("Arr: %d, Burst: %d, Rem: %d, Pri: %d",
-                 arrivalTime, burstTime, remainingTime, priority);
+        return String.format("Arr: %d, Burst: %d, Comp: %d",
+                arrivalTime, burstTime, completionTime);
     }
+
 }
--- Scheduler.java
+++ Scheduler.java
@@ -1,66 +1,112 @@
-import java.util.ArrayList;
-import java.util.Queue;
 import java.util.*;
 
-record Pair<PID, RemainingTime>(PID pid, RemainingTime remainingTime) {
+record Pair<PID, ArrivalTime>(PID pid, ArrivalTime arrivalTime) {
 }
 
-abstract class Scheduler {
+public abstract class Scheduler {
 
-    ArrayList<Process> processes;
+    PriorityQueue<Pair<Integer, Integer>> processQueue;
+    List<Process> processes;
 
     public Scheduler() {
-        processes = new ArrayList<Process>();
+        processQueue = new PriorityQueue<>(
+                Comparator.comparingInt(p -> p.arrivalTime()));
+        processes = new ArrayList<>();
     }
 
-    abstract void addProcess(Process p);
+    public static void main(String[] args) {
+        int currentTime = 0;
+        Scheduler sch = new FCFS();
+        sch.addProcess(new Process(5, 1, 0));
+        sch.addProcess(new Process(3, 1, 1));
+        sch.addProcess(new Process(8, 1, 2));
+        sch.readyProcess(currentTime);
+        while (currentTime < 20) {
+            int pid = sch.getNextProcess();
+            if (pid == -1) {
+                currentTime++;
+                sch.readyProcess(currentTime);
+                continue;
+            }
+            int t = sch.processes.get(pid).remainingTime;
+            for (int i = 0; i < t; i++) {
+                currentTime++;
+                sch.updateProcesses(pid, currentTime);
+                sch.readyProcess(currentTime);
+            }
 
-    abstract Process getNextProcess();
+        }
+        System.out.println();
+        for (int i = 0; i < sch.processes.size(); i++) {
+            System.out.println(sch.processes.get(i).toString());
+        }
+
+    }
+
+    void addProcess(Process p) {
+        processes.add(p);
+        processQueue.add(new Pair<>(processes.size() - 1, p.arrivalTime));
+
+    }
+
+    abstract void readyProcess(int currentTime);
+
+    abstract int getNextProcess();
+
+    abstract void updateProcesses(int pid, int currentTime);
 
 }
 
 class FCFS extends Scheduler {
-    Queue<Integer> processQueue;
+    public Queue<Integer> readyQueue;
 
     public FCFS() {
         super();
+        readyQueue = new LinkedList<>();
 
     }
 
     @Override
-    void addProcess(Process p) {
-        processes.add(p);
-        processQueue.add(processes.size() - 1);
+    void readyProcess(int currentTime) {
+        while (!processQueue.isEmpty() && currentTime == processQueue.peek().arrivalTime())
+            readyQueue.add(processQueue.poll().pid());
 
     }
 
     @Override
-    Process getNextProcess() {
-        return processes.get(processQueue.poll());
+    int getNextProcess() {
+        return readyQueue.isEmpty() ? -1 : readyQueue.poll();
+    }
+
+    @Override
+    void updateProcesses(int pid, int currentTime) {
+        processes.get(pid).update_remaining(currentTime);
+
     }
 
 }
-
-class SJF extends Scheduler {
-    PriorityQueue<Pair<Integer, Integer>> processQueue = new PriorityQueue<>(
-            Comparator.comparingInt(p -> p.remainingTime()));
-    boolean isPreemptive;
-
-    public SJF(boolean isPreemptive) {
-        super();
-        this.isPreemptive = isPreemptive;
-    }
-
-    @Override
-    void addProcess(Process p) {
-        processes.add(p);
-        processQueue.add(new Pair<>(processes.size() - 1, p.remainingTime));
-
-    }
-
-    @Override
-    Process getNextProcess() {
-        return processes.get(processQueue.poll().pid());
-    }
-
-}
+/*
+ * class SJF extends Scheduler {
+ * PriorityQueue<Pair<Integer, Integer>> processQueue = new PriorityQueue<>(
+ * Comparator.comparingInt(p -> p.remainingTime()));
+ * boolean isPreemptive;
+ * 
+ * public SJF(boolean isPreemptive) {
+ * super();
+ * this.isPreemptive = isPreemptive;
+ * }
+ * 
+ * @Override
+ * void addProcess(Process p) {
+ * processes.add(p);
+ * processQueue.add(new Pair<>(processes.size() - 1, p.remainingTime));
+ * 
+ * }
+ * 
+ * @Override
+ * Process getNextProcess() {
+ * return processes.get(processQueue.poll().pid());
+ * }
+ * 
+ * }
+ */
